d90907579a9e9440100bf2183dfb1fdb4311c60d
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-12-06 17:12:42 +0600

    update rider socket

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 export const apiPath = import.meta.env.VITE_API_ENDPOINT;
 
-export const socketServer = import.meta.env.VITE_APP_SOCKET_SERVER;
+export const socketServer = import.meta.env.VITE_SOCKET_SERVER;
 export const apiAuthToken = import.meta.env.VITE_API_AUTH_TOKEN;
